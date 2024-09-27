5c8bd8d294d15c7c242fe88c6264701e88b5d038
Samuel Batchelor (1000125268) <BATCSG1@student.op.ac.nz>
2024-09-27 14:34:52 +1200

    Footer added and peter image added

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let year = 2024;
+    let currentTime = new Date();
+    export let img;
+    export let alt;
+</script>
+
+<p>&copy; Web 1 {year}: The time is {currentTime}</p>
+<div>
+    
+</div>
+<style>
+    p{
+        background-color: chartreuse;
+        color: red;
+        padding: 2em;
+        font-weight: bold;
+        text-align: center;
+        font-size: large;
+        border: 5px dotted dodgerblue;
+
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
+<script>
+    import Footer from '$lib/Footer.svelte';
+    import Peter from '$lib/img/peter.jpeg';
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+
+
 <div>
-    <h2>I love frameworks</h2>
+    <h2>Frameworks are the bomb, bro!</h2>
 </div>
+
+<Footer year={2025} img ={Peter} alt="Peter"/>
+<Footer />
+<Footer year={1986}/>
+<Footer year={1999} />
+
+<style>
+    :global(*){
+        font-family: sans-serif;
+    }
+   
+</style>
