1929a14f2f2e18c84cfc049f45a4271b178f1daa
Samuel Batchelor (1000125268) <BATCSG1@student.op.ac.nz>
2024-10-15 11:12:19 +1300

    Created headingTitle variable, and styled header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,10 @@
 <script>
-
+    export let headingTitle;
 
 </script>
 
 <header>    
-    <h1>My Classwork website!</h1>
+    <h1>{headingTitle}</h1>
     <nav>
         <ul>
             <li><a href="/">Home</a></li>
@@ -15,6 +15,32 @@
 </header>
 
 <style>
-
+    header{
+        display: flex;
+        justify-content: center;
+    }
+    h1{
+        display: grid;
+        align-items: center;
+    }
+    ul{
+        display: flex;
+        justify-content: space-around;
+        list-style: none;
+        font-size: 1.5em;
+    }
+    ul li{
+        background-color: yellow;      
+        padding-inline: 3em;
+        padding-block: 1em;
+        border: 2px solid black;
+        
+    }
+    ul li a{
+        text-decoration: none; 
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        color: #333;
+    }
+  
 
 </style>
