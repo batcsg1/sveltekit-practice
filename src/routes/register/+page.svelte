ffaf8ed821290e75f78bcf82132c61a0e8fde6e0
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-15 20:48:26 +1300

    Updated register and about pages

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,7 +3,7 @@
 
 </script>
 
-<Header headingTitle = "My very amazing practice website!"></Header>
+<Header headingTitle = "Register"></Header>
 
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias totam veniam numquam assumenda provident, quas repellendus magnam, illo molestiae laborum nemo omnis ipsa doloremque enim. Repellat esse provident nesciunt quae!</p>
 
