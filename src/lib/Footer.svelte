ca847f5b7bdb99ebd0c4f3596cfed534352f2480
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-15 20:57:45 +1300

    Modified footer to include reserved rights details

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,17 +5,27 @@
     export let alt;
 </script>
 
-<p>&copy; Web 1 {year}: The time is {currentTime}</p>
-<div>
-    
-</div>
+<!-- <p>&copy; Web 1 {year}: The time is {currentTime}</p> -->
+
+<p>An OpenAI production</p>
+<p>ChatGPT &copy; 2006 </p>
+
 <style>
-    p{
+    /* p{
         background-color: rgb(0, 61, 49);
         color: rgb(248, 244, 244);
         padding: 2em;
         font-weight: bold;
         text-align: center;
         font-size: large;
+    } */
+    p{
+        font-size: 0.8em;
+        text-align: center;
+    }
+    p:first-of-type{
+        color: rgb(5, 186, 134);
+        font-weight: bold;
+        padding-top: 1em;
     }
 </style>
