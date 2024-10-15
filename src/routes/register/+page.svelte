cb28e90014d1b216bd9fbccc5acfffd9248a9b21
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-15 20:46:16 +1300

    Added register page

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from '$lib/Header.svelte';
+
+</script>
+
+<Header headingTitle = "My very amazing practice website!"></Header>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias totam veniam numquam assumenda provident, quas repellendus magnam, illo molestiae laborum nemo omnis ipsa doloremque enim. Repellat esse provident nesciunt quae!</p>
+
+
+
+<style>
+
+
+
+</style>
