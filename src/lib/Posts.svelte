2680f62a58f5de17c23932b5483dd68fc039c0e3
batcsg1 <BATCSG1@student.op.ac.nz>
2024-10-25 14:14:41 +1300

    Update

--- src/lib/Posts.svelte
+++ src/lib/Posts.svelte
@@ -1,7 +1,16 @@
 <script>
-
+    export let post;
 </script>
 
-<style>
+    <article>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+    </article>
 
+<style>
+    article{
+        background-color: rgb(228, 216, 216);
+        padding: 0.5em ;
+        margin: 1em;
+    }
 </style>
