ee214d5ec9f037397cef2b681f9335b22bd2bcf5
batcsg1 <BATCSG1@student.op.ac.nz>
2024-10-25 13:34:57 +1300

    Filtered data to posts data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Header from "$lib/Header.svelte";
   import Main from "$lib/Main.svelte";
   export let data;
-  console.log(data);
+  console.log(data.posts);
 </script>
 
 <Header headingTitle = "chatgpt.com"></Header>
