d101e4aff465bca5b81473a4b99ea622d88ef4eb
batcsg1 <BATCSG1@student.op.ac.nz>
2024-10-25 13:47:11 +1300

    Added styling for post article

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,26 @@
   import Header from "$lib/Header.svelte";
   import Main from "$lib/Main.svelte";
   export let data;
-  console.log(data.posts);
+  const posts = data.posts;
+
 </script>
 
 <Header headingTitle = "chatgpt.com"></Header>
 
 <Main mainTitle = "Welcome to ChatGPT!"></Main>
+<h2>Random posts</h2>
+{#each posts as post }
+    <article>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+    </article>
+{/each}
+
+<style>
+    article{
+        background-color: rgb(228, 216, 216);
+        padding: 0.5em ;
+        margin: 1em;
+    }
+</style>
 
