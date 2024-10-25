f4741ae05c4d289a769d18a72e98596eec913c33
batcsg1 <BATCSG1@student.op.ac.nz>
2024-10-25 14:14:11 +1300

    Changed structure of each loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "$lib/Header.svelte";
   import Main from "$lib/Main.svelte";
+  import Posts from "$lib/Posts.svelte";
   export let data;
   const posts = data.posts;
 
@@ -10,18 +11,12 @@
 
 <Main mainTitle = "Welcome to ChatGPT!"></Main>
 <h2>Random posts</h2>
-{#each posts as post }
-    <article>
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-    </article>
+{#each posts as post}
+    <Posts {post}/>
 {/each}
 
+
 <style>
-    article{
-        background-color: rgb(228, 216, 216);
-        padding: 0.5em ;
-        margin: 1em;
-    }
+    
 </style>
 
