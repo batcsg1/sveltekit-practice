271b5f0890f08b4a26b5793f8f08bcb393545ac1
batcsg1 <BATCSG1@student.op.ac.nz>
2024-10-29 11:00:31 +1300

    Added counter component

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+    let count = 0;
+    function incrementCount(){
+        count++;
+    }
+</script>
+    <button on:click={incrementCount}>Add +1</button>
+    <p>Count: {count}</p>
+<style>
+
+</style>
